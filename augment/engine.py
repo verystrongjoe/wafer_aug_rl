925b279a7071edf334fc4887921cf18e586f56da
verystrongjoe <verystrongjoe@gmail.com>
2022-12-13 00:25:24 +0900

    temporary

--- augment/engine.py
+++ augment/engine.py
@@ -172,9 +172,6 @@ if __name__ == '__main__':
 
     args.logger.info(f'train data of {len(train_set)} is augmented by best policy. start...')
 
-    from multiprocessing import set_start_method
-    set_start_method("spawn")
-
     # generate train dataset augmented by best policy above
     # with Pool(args.num_workers) as p:
     #     r = p.starmap(augment_by_policy_wapirl, product(train_set.samples, [eval_policy], [args]))
--- run_eval.sh
+++ run_eval.sh
@@ -1,2 +1,4 @@
 python -m eval_policy --project_name deep_augment --num_worker 125 > output_eval.log 2>&1 &
 # nohup python -m augment.engine --project_name deep_augment --num_worker 125
+
+# python -m augment.engine --project_name deep_augment --num_worker 120 --opt_iterations 200  --opt_samples 3  --epochs 100
